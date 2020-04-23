14246c88ab96b13cc7d3446327c26b22bae8c545
lishaowei <lishaowei@quicklyun.com>
2020-04-23 16:32:02 +0800

    master httpget addHeader user-agent

--- craw/src/main/java/com/lsw/craw/util/HttpClientUtil.java
+++ craw/src/main/java/com/lsw/craw/util/HttpClientUtil.java
@@ -10,8 +10,11 @@ import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
 import org.apache.http.util.EntityUtils;
 import org.springframework.stereotype.Component;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.util.UUID;
 
 @Component
 public class HttpClientUtil {
@@ -35,6 +38,8 @@ public class HttpClientUtil {
         HttpGet httpGet = new HttpGet(url);
         // 设置请求参数
         httpGet.setConfig(this.getRequestConfig());
+        // todo 重要 设置请求Request Headers中的User-Agent，告诉京东说我这是浏览器访问，
+        httpGet.addHeader("User-Agent","Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36");
         // 执行请求
         CloseableHttpResponse response = null;
         String content = "";
@@ -43,6 +48,7 @@ public class HttpClientUtil {
             if (response.getStatusLine().getStatusCode() == 200) {
                 // 得到响应
                 HttpEntity entity = response.getEntity();
+                assert entity != null;
                 content = EntityUtils.toString(entity, StandardCharsets.UTF_8);
             }
         } catch (IOException e) {
@@ -67,15 +73,25 @@ public class HttpClientUtil {
         httpGet.setConfig(this.getRequestConfig());
         // 执行请求
         CloseableHttpResponse response = null;
-        String content = "";
+        String imgName = "";
         try {
             response = httpClient.execute(httpGet);
             if (response.getStatusLine().getStatusCode() == 200) {
                 // todo 这里主要是图片的处理不一样，下载图片，生成图片名字，图片后缀，图片地址
+                // 扩展名
+                String imgType = url.substring(url.lastIndexOf("."));
+                // 图片名
+                imgName = UUID.randomUUID().toString() + imgType;
+                // 下载图片
+                FileOutputStream fos = new FileOutputStream(new File(""));
+                response.getEntity().writeTo(fos);
+                fos.close();
+                // 返回图片名 return
             }
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
+
             try {
                 assert response != null;
                 response.close();
@@ -83,7 +99,7 @@ public class HttpClientUtil {
                 e.printStackTrace();
             }
         }
-        return content;
+        return imgName;
     }
 
     private RequestConfig getRequestConfig(){
@@ -92,5 +108,12 @@ public class HttpClientUtil {
                 .setConnectionRequestTimeout(500) // 获取连接请求的时长
                 .setSocketTimeout(1000).build();// 数据传输超时时长
         return build;
+    }
+
+    public static void main(String[] args) {
+        String s = "www.baidu.com/111.jpg";
+        int ss = s.lastIndexOf(".");
+        String rr = s.substring(ss);
+        System.out.println("rr = " + rr);
     }
 }
